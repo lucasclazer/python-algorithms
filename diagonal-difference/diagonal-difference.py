d1a3673d149e3795def175f4f7bf2b7ed04fe44b
Lucas Clazer <lucas.perussolo@evoluum.com.br>
2021-05-12 00:26:50 -0300

    Solving 2d array diagonal difference.

--- diagonal-difference/diagonal-difference.py
+++ diagonal-difference/diagonal-difference.py
@@ -1,6 +1,37 @@
+#first
+# accessPosition [0][0]
+# accessPosition [1][1]
+# accessPosition [2][2]
+
+#second
+# accessPosition [0][2]
+# accessPosition [1][1]
+# accessPosition [2][0]
 
 def diagonalDifference(arr):
-    return ''
+    ans=0
+    first=0
+    second=0
+    
+    length = len(arr)
+    for i in range(length):
+        first += arr[i][i]
+        second += arr[i][-1 * i + (length-1)]
+    
+    ans = first - second
 
+    if ans > 0:    
+        return ans
+    elif ans < 0:    
+        return -1*ans
+    
+    return 0
+    
 if __name__ == '__main__':
- arr = 
+    arr = [[11, 2, 4],
+        [4, 5, 6],
+        [10, 8, -12]]
+    print(diagonalDifference(arr))
+
+
+
