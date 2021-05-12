e2f22cd02f2201fd51f4c769cc72abf3c14f52b0
Lucas Clazer <lucas.perussolo@evoluum.com.br>
2021-05-11 22:33:15 -0300

    Initializing gitignore and project.

---
+++ simple-array-sum/array_sum.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+
+def simpleArraySum(ar):
+    # Write your code here
+    print("Hello World test", ar)
+
+
+if __name__ == '__main__':
+    simpleArraySum("abc abc")
